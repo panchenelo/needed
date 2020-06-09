e7af7c6972044ebec8b6f3ca39da4402c1ede7f0
panchenelo <45730607+panchenelo@users.noreply.github.com>
2020-06-09 19:39:20 +0300

    Create countrieslist.js

---
+++ countrieslist.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        pl: {
+            name: 'Polska',
+            code: 'pl',
+            price: 159,
+            oldPrice: 318,
+            labelPrice: 'zl',
+            phoneHelper: 'Na przyklad, +48 123456789',
+            nameHelper: 'Tom Czerniak',
+            nameError: 'Imie (wymagane)',
+            phoneError: 'Blednie podany numer',
+            countryError: 'Kraj (wymagany)',
+            isDefault: true
+        }
+    };
+})();
